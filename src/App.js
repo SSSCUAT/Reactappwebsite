2cc9e2eb450bd74c8fa61531b6f1c54fd18efa4e
SSSCUAT <reynosodiego264@gmail.com>
2024-07-25 00:02:04 -0700

    Dynamic textcolor with user input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 function App() {
+  //start of dynamic website 
+  const[textcolor, setcolor] =useState('black') //defines Textcolor (idk how)
+
+  useEffect( //Helps define the event listeners  
+    () => { 
+      const handleKeydown= (event)=> { //Handles key down 
+        if (event.key === 'a') {setcolor('blue')} //Tells us which key and what to do 
+        else if (event.key === 'd') {setcolor('red')}  //alt
+      } 
+      
+      const handleKeyup= () => { // Handles key up always black doesnt matter which key is released 
+        setcolor('black')
+      }
+      window.addEventListener('keydown', handleKeydown) //checks for event
+      window.addEventListener('keyup', handleKeyup) //checks for key up event
+      return () => {
+        window.removeEventListener('keydown', handleKeydown)
+        window.removeEventListener('keyup', handleKeyup)
+      }
+    } ,[] //No dependanies mean that this function only gets called once
+    );
+  // end of dynamic website 
   return (
     <div className="App">
       <div className='First-Row'>
         <div className='Intro-Contaianer'>
           <img className='Personal-Photo' src='Diego.jpg' alt='Personal Photo'></img>
-          <h2>Diego Reynoso</h2>
+          <h2 style = {{color: textcolor }} >Diego Reynoso  </h2>
+           {/* Added dynamic styling to div*/}
+
           <p1>Incoming Transfer Student from East Los Angeles College </p1>
         </div>
         <div className='Links-Container'>
